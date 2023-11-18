5c78cda8d4723d68a5c6330f33a41f48112264d5
Sqtell1iite <pavaliew.71a@gmail.com>
2023-11-18 22:33:34 +0400

    Update loginform.py

--- forms/loginform.py
+++ forms/loginform.py
@@ -7,5 +7,4 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     login_or_email = StringField('Логин или адрес электронной почты', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
