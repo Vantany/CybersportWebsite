25c8217bea760dc558134510b2aab5f698f76337
Sqtell1iite <pavaliew.71a@gmail.com>
2023-11-18 21:53:21 +0400

    add: templates folder; red: main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = 'oaoaooa'
+app.config["JSON_AS_ASCII"] = False
+
+
+@app.route("/")
+def index():
+    return "<h1>IN PROGRESS</h1>"
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
